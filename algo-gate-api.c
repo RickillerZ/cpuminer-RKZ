15f0eb613aef7da1b94ca543b4592062dba93864
RickillerZ <46547196+RickillerZ@users.noreply.github.com>
2019-08-20 22:38:45 -0400

    Add support LightBit

--- algo-gate-api.c
+++ algo-gate-api.c
@@ -246,6 +246,7 @@ bool register_algo_gate( int algo, algo_gate_t *gate )
     case ALGO_YESPOWER_URX:  register_yespower_urx_algo  ( gate ); break;
     case ALGO_CPUPOWER:      register_cpupower_algo      ( gate ); break;
     case ALGO_YESPOWER_LTNCG: register_yespower_ltncg_algo ( gate ); break;
+    case ALGO_YESPOWERLITB:  register_yespowerlitb_algo  ( gate ); break;
     case ALGO_YESPOWER:      register_yespower_algo      ( gate ); break;
     case ALGO_YESPOWERR16:   register_yespowerr16_algo   ( gate ); break;
     case ALGO_ZR5:           register_zr5_algo           ( gate ); break;
